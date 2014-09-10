d4907da2ea6d51aebed657356ae08083c05fbff7
Dhole <bankukur@gmail.com>
2014-09-10 19:14:22 +0200

    modified:   wii_controller.py

--- wii_controller.py
+++ wii_controller.py
@@ -2,6 +2,7 @@
 
 import cwiid
 import time
+import RPIO
 from RPIO import PWM
 
 MIN_PULSE = 0
@@ -14,11 +15,31 @@ PIN_B = 17
 PIN_L = 27
 PIN_R = 22
 
+CHAN_F = 0
+CHAN_B = 1
+
+def set_speed(pin, chan, speed, speed_0):
+
+    pulse_inc = PWM.get_pulse_incr_us()
+    cycle_dur = PWM.get_channel_subcycle_time_us(chan)
+    num_pulses = int(cycle_dur * speed / pulse_inc)
+
+    PWM.add_channel_pulse(chan, pin, 0, num_pulses)
+    
+
 PWM.set_loglevel(PWM.LOG_LEVEL_ERRORS)
 
+RPIO.setup(PIN_L, RPIO.OUT)
+RPIO.setup(PIN_R, RPIO.OUT)
+RPIO.output(PIN_L, False)
+RPIO.output(PIN_R, False)
+
 PWM.setup()
-PWM.init_channel(0, subcycle_time_us = PWM_SUBCYCLE)
-PWM.add_channel_pulse(0, PIN_F, 0, 0)
+PWM.init_channel(CHAN_F, subcycle_time_us = PWM_SUBCYCLE)
+PWM.init_channel(CHAN_B, subcycle_time_us = PWM_SUBCYCLE)
+PWM.add_channel_pulse(CHAN_F, PIN_F, 0, 0)
+PWM.add_channel_pulse(CHAN_B, PIN_B, 0, 0)
+
 #servo = PWM.Servo()
 #servo.set_servo(PIN_F, 0)
 
@@ -35,13 +56,27 @@ print 'Wiimote connected'
 wm.led = 1
 wm.rpt_mode = cwiid.RPT_BTN
 
-speed = 0
-speed_0 = 0
+speed = 0.6
+speed_0 = speed
+move = '0'
+move_0 = move
+turn = '0'
+turn_0 = turn
 
 while True:
+    move = '0'
+    turn = '0'
     if wm.state['buttons'] & cwiid.BTN_UP:
+        turn = 'l'
+    if wm.state['buttons'] & cwiid.BTN_DOWN:
+        turn = 'r'
+    if wm.state['buttons'] & cwiid.BTN_1:
+        turn = 'b'
+    if wm.state['buttons'] & cwiid.BTN_2:
+        move = 'f'
+    if wm.state['buttons'] & cwiid.BTN_PLUS:
         speed += 0.1
-    elif wm.state['buttons'] & cwiid.BTN_DOWN:
+    if wm.state['buttons'] & cwiid.BTN_MINUS:
         speed -= 0.1
 
     if speed != speed_0:
@@ -50,16 +85,30 @@ while True:
         elif speed > 1:
             speed = 1
 
-        pulse_w = MIN_PULSE + speed * (MAX_PULSE - MIN_PULSE)
-        # ??? using low level PWM, max becomes 2000
-        pulse_w /= 10
-        pulse_w -= pulse_w % PWM_PULSE_INC
-        print pulse_w
-        #PWM.clear_channel_gpio(0, PIN_F)
-        PWM.add_channel_pulse(0, PIN_F, 0, int(pulse_w))
-        #servo.stop_servo(PIN_F)
-        #servo.set_servo(PIN_F, pulse_w)
-        speed_0 = speed
+    if (move != move_0):
+        if move == 'f':
+            set_speed(PIN_B, CHAN_B, 0, 1)
+            set_speed(PIN_F, CHAN_F, speed, speed_0)
+        elif move == 'b':
+            set_speed(PIN_F, CHAN_F, 0, 1)
+            set_speed(PIN_B, CHAN_B, speed, speed_0)
+        elif move == '0':
+            set_speed(PIN_B, CHAN_F, 0, 1)
+            set_speed(PIN_F, CHAN_F, 0, 1)
 
+    if (turn != turn_0):
+        if turn == 'l':
+            RPIO.output(PIN_L, True)
+            RPIO.output(PIN_R, False)
+        elif turn = 'r':
+            RPIO.output(PIN_L, False)
+            RPIO.output(PIN_R, True)
+        elif turn = '0':
+            RPIO.output(PIN_L, False)
+            RPIO.output(PIN_R, False)
+        
+    move_0 = move
+    turn_0 = turn
+    speed_0 = speed
     time.sleep(0.1)
 
